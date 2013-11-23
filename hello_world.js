69161b5afe4e755932d8c83dc50c5222531d86f8
johnnymo87 <johnnymo87@gmail.com>
2013-11-23 20:17:21 +0000

    init

---
+++ hello_world.js
@@ -0,0 +1,15 @@
+/**
+ * Created by saasbook on 11/22/13.
+ */
+console.log("Hello World");
+
+function say(word) {
+    console.log(word);
+}
+
+function execute(someFunction, value) {
+    someFunction(value);
+}
+
+execute(say, 'Hello');
+execute(function(word){ console.log(word) }, 'Hello from anon!')
